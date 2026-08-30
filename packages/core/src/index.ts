0bc47dafc40befc4ed90af7d2044fd205ea5f580
Mico-Liu <xiaokexuejia@gmail.com>
2026-08-30 09:22:07 +0800

    feat: implement CaseRuntime v0

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,16 @@
 export const CORE_VERSION = '0.0.1'
+
+export {
+  CASE_RUNTIME_PHASES,
+  CASE_RUNTIME_TRANSITIONS,
+  restoreCaseRuntime,
+  serializeCaseRuntime,
+  transitionCaseRuntime,
+  type CaseRuntimePhase,
+  type CaseRuntimeState,
+  type InvalidCaseRuntimeDataError,
+  type InvalidTransitionError,
+  type RestoreCaseRuntimeResult,
+  type SerializedCaseRuntimeState,
+  type TransitionResult,
+} from './case/case-runtime'
